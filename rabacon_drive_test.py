1a6f68fabba9fca641c6ab06a047b50bca774bf1
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-08-10 18:45:24 +0900

    Update rabacon_drive_test.py

--- rabacon_drive_test.py
+++ rabacon_drive_test.py
@@ -22,26 +22,21 @@ class ClusterLidar :
         rospy.Subscriber("/obstacles", Obstacles, self.rabacon)
         self.rabacon_pub = rospy.Publisher("rabacon_drive", Float32, queue_size = 5)
         self.angle = 0.0 
-        self.is_rabacon_mission = False
 
     # select close rabacon
     def rabacon(self, _data)  :
-        left_rabacons, right_rabacons = self.sel_close_rabacon(_data)
-        self.drive_angle = self.cal_angle(left_rabacons, right_rabacons)
         if len(_data) >= 6 :
-            self.is_rabacon_mission = True
+            left_rabacons, right_rabacons = self.sel_close_rabacon(_data)
+            self.drive_angle = self.cal_angle(left_rabacons, right_rabacons)
+            self.rabacon_pub.publish(self.drive_angle)
         else :
-            self.is_rabacon_mission = False
-        return self.is_rabacon_mission, self.drive_angle * 0.017
+            self.rabacon_pub.publish(1000)
 
     def cal_angle(self, left_rabacons, right_rabacons) :
         left_angle = abs(math.atan(left_rabacons[0].center.y/left_rabacons[0].center.x))
         right_angle =  abs(math.atan(right_rabacons[0].center.y/right_rabacons[0].center.x))
+        return (left_angle - right_angle)*0.0174
 
-        if self.is_rabacon_mission == True :
-            self.rabacon_pub.publish((left_angle - right_angle)*0.0174)
-            sleep(0.03)
-        
 
     def sel_close_rabacon(self, rabacons) :
         left_rabacon = []
@@ -54,7 +49,7 @@ class ClusterLidar :
         left_rabacon = sorted(left_rabacon, key = lambda x : x.center.x)[:2]
         right_rabacon = sorted(right_rabacon, key = lambda x : x.center.x)[:2]
 
-        return left_rabacon, 
+        return left_rabacon, right_rabacon
 
 def run() :
     rospy.init_node("rabacon_drive")
